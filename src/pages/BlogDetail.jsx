0ac10b799a9f230d916ac59170c52cda0efc01b4
ManojGowda89 <mail@manojgowda.in>
2025-09-17 14:22:45 +0530

    dsoj

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -22,7 +22,7 @@ const BlogDetail = () => {
   useEffect(() => {
     const fetchBlog = async () => {
       try {
-        const res = await axios.get(`http://localhost:5000/api/blogs/${id}`);
+        const res = await axios.get(`https://api-email.manojgowda.in/api/blogs/${id}`);
         setBlog(res.data);
       } catch (err) {
         console.error("Error fetching blog:", err);
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -21,7 +21,7 @@ const Blogs = () => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/blogs");
+        const res = await axios.get("https://api-email.manojgowda.in/api/blogs");
         setBlogs(res.data);
       } catch (err) {
         console.error("Error fetching blogs:", err);
